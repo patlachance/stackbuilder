884a15dec3801be6abac6c2cc53dc53f059823f1
David Ellis <dellis@ldn-lap-dellis.youdevise.com>
2013-09-26 17:51:13 +0100

    dellis: wip

--- spec/proxy_spec.rb
+++ spec/proxy_spec.rb
@@ -8,6 +8,10 @@ describe_stack 'exampleproxy' do
         end
         sso_vhost('exampleapp') do
         end
+        vhost('exampleapp2') do
+        end
+        sso_vhost('exampleapp2') do
+        end
         enable_nat
         self.ports = [80,443,8443]
       end
@@ -17,11 +21,15 @@ describe_stack 'exampleproxy' do
         self.application = 'example'
       end
 
+      virtual_appserver 'exampleapp2' do
+        self.groups = ['blue']
+        self.application = 'example'
+      end
     end
 
-    env "e1", :primary_site=>"space" do
-#      'exampleproxy.vhost.exampleapp.server_name' => 'example.overridden',
-#      'exampleproxy.vhost.exampleapp-sso.server_name' => 'example-sso.overridden' do
+    env "e1", :primary_site=>"space",
+      'exampleproxy.vhost.exampleapp2.server_name' => 'example.overridden',
+      'exampleproxy.vhost.exampleapp2-sso.server_name' => 'example-sso.overridden' do
       instantiate_stack "exampleproxy"
       end
   end
@@ -29,13 +37,14 @@ describe_stack 'exampleproxy' do
   host("e1-exampleproxy-001.mgmt.space.net.local") do |host|
     host.to_enc.should eql(
       {"role::proxyserver"=>{
-      "vhosts"=>{"e1-exampleproxy-vip.front.space.net.local"=>{
-      "proxy_pass_rules"=>{
-      "/"=>"http://e1-exampleapp-vip.space.net.local:8000"},
-      "aliases"=>["e1-exampleproxy-vip.front.space.net.local",
-        "e1-exampleproxy-vip.space.net.local"],
-        "application"=>"example",
-        "redirects"=>[]},
+      "vhosts"=>{
+        "e1-exampleproxy-vip.front.space.net.local"=>{
+          "proxy_pass_rules"=>{
+            "/"=>"http://e1-exampleapp-vip.space.net.local:8000"
+          },
+          "aliases"=>["e1-exampleproxy-vip.space.net.local"],
+          "application"=>"example",
+          "redirects"=>[]},
 
         "e1-exampleproxy-sso-vip.front.space.net.local"=>{
           "proxy_pass_rules"=>{
@@ -43,8 +52,24 @@ describe_stack 'exampleproxy' do
             "aliases"=>[
             "e1-exampleproxy-vip.space.net.local"],
             "application"=>"example",
-            "redirects"=>[]}},
+            "redirects"=>[]},
 
+        "example.overridden"=>{
+          "proxy_pass_rules"=>{
+            "/"=>"http://e1-exampleapp2-vip.space.net.local:8000"
+          },
+          "aliases"=>["e1-exampleproxy-vip.front.space.net.local", "e1-exampleproxy-vip.space.net.local"],
+          "application"=>"example",
+          "redirects"=>[]},
+
+        "example-sso.overridden"=>{
+          "proxy_pass_rules"=>{
+            "/"=>"http://e1-exampleapp2-vip.space.net.local:8000"},
+            "aliases"=>[
+              "e1-exampleproxy-sso-vip.front.space.net.local",
+              "e1-exampleproxy-vip.space.net.local"],
+            "application"=>"example",
+            "redirects"=>[]}},
         "prod_vip_fqdn"=>"e1-exampleproxy-vip.space.net.local"}})
   end
 end
